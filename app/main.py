21522fc9754c4edd409c7f54e91068e45bcfe5fb
Justin Miller <justinrmiller@gmail.com>
2020-12-15 02:22:46 -0800

    Getting test coverage to 100%

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 
+from loguru import logger
+
 app = FastAPI()
 
 
 @app.get("/")
 def root():
+    logger.info("root called")
     return {"msg": "Hello World"}
 
 
 @app.get("/healthcheck")
 def healthcheck():
+    logger.info("healthcheck called")
     return {"status": "OK"}
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,9 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+
+def test_healthcheck():
+    response = client.get("/healthcheck")
+    assert response.status_code == 200
+    assert response.json() == {'status': 'OK'}
